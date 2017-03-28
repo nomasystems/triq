c951d710909f5889b7f029aa5b80f57b02e29818
Peter Lemenkov <lemenkov@gmail.com>
2017-03-28 17:51:58 +0300

    Spec matches actual function
    INFO:  Regenerating edocs for triq
    ./src/triq_dom.erl, function atom/0: at line 797: @spec name does not match.
    edoc: skipping source file './src/triq_dom.erl': {'EXIT',error}
    
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/triq_dom.erl
+++ src/triq_dom.erl
@@ -794,7 +794,7 @@ bitstring_shrink(#?DOM{kind=#bitstring{size=Size}, empty_ok=EmptyOK}=BinDom,
     end.
 
 %% @doc The domain of atoms
-%% @spec int() -> domain(integer())
+%% @spec atom() -> domain(integer())
 -spec atom() -> domrec(atom()).
 atom() ->
     #?DOM{kind=#atom{size=any},
